0ea38a7dfb7f02689e120b56da1db0864b3eb7f8
EvandoNeto <89491970+EvandoNeto@users.noreply.github.com>
2021-10-26 11:43:34 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,13 @@
 
-class Pessoa:
-    def __init__(self,nome,idade,sexo,cidade,estado):
-        self.nome = nome
-        self.idade = idade
-        self.sexo = sexo
-        self.cidade = cidade
-        self.estado = estado
+from Pessoa import Pessoa
 
-    def get_nome(self):
-        return self.__nome
-
-    def set_nome(self, nome):
-        self.__nome = nome
-
-    def get_idade(self):
-        return self.__idade
-
-    def set_nome(self, idade):
-        self.__idade = idade
-
-    def get_sexo(self):
-        return self.__sexo
-
-    def set_nome(self, sexo):
-        self.__sexo = sexo
-
-    def get_cidade(self):
-        return self.__cidade
-
-    def set_nome(self, cidade):
-        self.__cidade = cidade
-
-    def get_estado(self):
-        return self.__estado
-
-    def set_nome(self, estado):
-        self.__estado = estado
-
+def pedir_dados():
+    nome = input('Escreva seu nome: ')
+    idade = input('Escreva sua idade: ')
+    sexo = input('Qual seu sexo: ')
+    cidade = input('Qual o nome da Cidade onde você mora: ')
+    estado = input('Qual o nome do Estado (UF) onde você mora: ')
+    return Pessoa(nome, idade, sexo, cidade, estado)
 
 if __name__ == "__main__":
-    pessoa1 = Pessoa(input('Qual o seu nome: '),input('Qual a sua idade: '),input('Qual o seu sexo: '),input('Qual a Cidade onde você mora: '),input('Qual a Estado(UF) onde você mora: '))
-    print(pessoa1.nome,",", pessoa1.idade,",", pessoa1.sexo,",", pessoa1.cidade,",",pessoa1.estado)
+    pedir_dados()
